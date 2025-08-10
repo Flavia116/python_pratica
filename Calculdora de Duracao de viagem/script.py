25e8312b1f67931efbbb4cbcc41f877902a1aa56
Flavia Araujo <flaviaaraujo@flavias-air.home>
2025-08-10 00:10:46 -0300

    python_pratica

---
+++ Calculdora de Duracao de viagem/script.py
@@ -0,0 +1,62 @@
+
+###Calculadora de Duração de Viagem
+
+#você precisa calcular o número total de horas de uma viagem, expressa em dias e horas. Uma viagem para florianopólis
+#dura 2 dias e 5 horas, por exemplo. Qual é a duração total dessa viagem em números de horas?
+
+nome = input(" informe seu nome [AQUI] :  ")
+horas_dias = int(input(" insira quantidade de dias :  "))
+horas_horas = int(input(" insira quantidade de horas : "))
+
+completa_V = int((horas_dias * 24 ) + horas_horas)
+
+print(f" Olá, {nome}. O total de horas da sua viagem é {completa_V}. obrigada por viajar com gente S2 . ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
